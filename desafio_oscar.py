6213eafb433e35e9573d6b24703d6df707452083
Clenylson Perycles <clenylson.perycles@gmail.com>
2025-04-25 07:23:04 -0300

    Iniciando criação de funções para tratamento de dados

--- desafio_oscar.py
+++ desafio_oscar.py
@@ -2,6 +2,34 @@ import pandas as pd
 
 #leitura do arquivo CSV, com definição de separador tab
 arquivo = pd.read_csv("datasheet_oscars.csv", sep="\t")
+#padronização do nome das colunas para minúsculo, retirando espaços antes e depois do nome, se houver
+arquivo.columns = arquivo.columns.str.lower().str.strip()
 
+
+def tbl_movie():
+    pass
+
+def tlb_oscar():
+    pass
+
+def tbl_class():
+    pass
+
+def tbl_category():
+    pass
+
+def tbl_nominees():
+    pass
+
+#iterando sobre as linhas da tabela a ser importada
 for linha in arquivo.itertuples(index=False):
-    print(linha.Year)
+    year = linha.year
+    category = linha.category
+    movie = linha.movie
+    name = linha.name
+    nominees = linha.nominees
+    winner = linha.winner
+    detail = linha.detail
+    note = linha.note
+
+    print(year, movie)
